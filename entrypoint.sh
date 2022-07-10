d31330a24e6362a8106d0fb58dfb6b99db355210
eduzi1784 <90742956+eduzi1784@users.noreply.github.com>
2022-07-10 02:23:56 +0000

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+UUID=719cf1a5-954b-4b19-b942-5be1e98ab06f
+WSPATH=/d720987c-9e70-43ad-a146-5ed5aea73fc6
+PORT=80
+
+
+# Download and install xray
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+
+# Remove temporary directory
+rm -rf /tmp/xray
+
+# xray new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+  "log": {
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "port": ${PORT},
+      "protocol": "VLESS",
+      "settings": {
+        "clients": [
+          {
+            "id": "${UUID}",
+            "alterId": 0
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "${WSPATH}"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+# Run xray
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
